2a4e9d0ea5a68e01f2d60329bcf018b28493e207
Andres Rossi <andresrossi2022@gmail.com>
2022-10-28 16:07:46 -0300

    visualizacion sitio 1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from "react";
 import Footer from "./components/layout/Footer";
 import HomePage from "./pages/HomePage";
 import ContactoPage from "./pages/ContactoPage";
-import Header from "./components/layout/Header/Header";
+import Header from "./components/layout/Header/Header"; 
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <HomePage />
-      <ContactoPage />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="contacto" element={<ContactoPage />} />
+          <Route path="nosotros" element={<NosotrosPage />} />
+          <Route path="galeria" element={<GaleriaPage />} />
+        </Routes>
+      </BrowserRouter>
+
       <Footer />
     </div>
   );
